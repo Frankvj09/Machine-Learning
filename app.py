c17d711de128c4f9ec86846e1721827c5706f477
Frankvj09 <91689603+Frankvj09@users.noreply.github.com>
2026-02-28 09:33:33 -0500

    Cao de uso machine learning

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", nombre="Francisco", edad=25, items=["Manzana", "Banana", "Naranja"])
+    return render_template("index.html", nombre="Francisco", edad=25, items=["Manzana", "Banana", "Naranja"])  
 
-
+@app.route("/page_one")
+def page_one():
+    return render_template("pagina_one.html", nombre="Supervisado")
 
 if __name__ == "__main__":
     app.run(debug=True)
